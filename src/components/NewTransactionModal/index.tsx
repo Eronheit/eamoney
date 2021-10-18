8692dc7220531ee44672ddadfb143a07e9f97cfd
Eronheit <aquinoeronaldo@gmail.com>
2021-10-18 12:50:39 -0300

    feat: create buttons of type transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from 'react-modal';
 
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -16,7 +18,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button type="button" onClick={onRequestClose} className="react-modal-close">
+      <button 
+        type="button" 
+        onClick={onRequestClose} 
+        className="react-modal-close"
+      >
         <img src={closeImg} alt="Fechar modal" />
       </button>
       <Container>
@@ -25,6 +31,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input type="text" placeholder="Título" />
 
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input type="text" placeholder="Categoria" />
 
